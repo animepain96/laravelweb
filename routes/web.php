651474a2b8959677af3898cd493c0726daef0293
AnimePain <animepain.96@gmail.com>
2019-08-29 12:31:43 +0700

    routes lession

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,38 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Route::get('/hello', function () {
+//     echo "<h1>Hello World!</h1>";
+// });
+
+// Route::get('/hello-world', function () {
+//     echo "<h1>Hello World!</h1>";
+// });
+
+//Truyen tham so
+// Route::get('/hello/{name}', function ($name) {
+//     echo '<h1>Hello Mr. ' . $name . '</h1>';
+// });
+
+// Route::get('/hello/{name}/{year}', function ($name, $year) {
+//     echo '<h1>Hello Mr. ' . $name . '</h1>';
+//     echo '<p>You are <b>' . $year . '</b> year olds</p>';
+// });
+
+// Route::get('/hello/{name}/{year?}', function ($name, $year=26) {
+//     echo '<h1>Hello Mr. ' . $name . '</h1>';
+//     echo '<p>You are <b>' . $year . '</b> year olds</p>';
+// });
+
+// Tham so co the khong truyen vao
+// Route::get('hello/{name}/{year?}', function ($name,$year=-1) {
+//     $result = $year==-1 ? 'Hello <b> ' . $name . '</b>' 
+//         : 'Hello <b> ' . $name . '</b><p>You are ' . $year . ' year olds.</p>';
+//     echo $result;
+// });
+
+//Rang buoc cac tham so truyen vao
+Route::get('cong/{num1}/{num2}', function ($num1, $num2) {
+    return $num1 + $num2;
+})->where(['num1' => '[0-9]+', 'num2' => '[0-9]+']);
